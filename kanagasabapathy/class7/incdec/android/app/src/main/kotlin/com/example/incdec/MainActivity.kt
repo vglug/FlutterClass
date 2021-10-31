0f06a9d8f358a0f0a32b4de17434025639129a2f
Kanagasabapathy R <saba09102001@gmail.com>
2021-10-31 12:22:06 +0530

    class7th Increment Decrement

---
+++ kanagasabapathy/class7/incdec/android/app/src/main/kotlin/com/example/incdec/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.incdec
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ kanagasabapathy/class7/incdec/lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      
+      _counter++;
+    });
+  }
+   void _decrementCounter() {
+    setState(() {
+      
+      _counter--;
+    });
+  }
+   void _refresh() {
+    setState(() {
+      
+      _counter=0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        
+        title: Text(widget.title),
+      ),
+      body: Center(
+        
+        child: Column(
+         
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.red) ,
+                  ),
+                  onPressed: _incrementCounter,
+                  child: Text('Increment'),
+                ),
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+                  ),
+                  onPressed: _decrementCounter,
+                  child: Text('Decrement'),
+                ),
+              ],
+            ),
+             
+          ],
+        ),
+      ),
+        floatingActionButton: FloatingActionButton(
+             
+              onPressed: _refresh,
+              tooltip: 'Refresh',
+              child: Icon(Icons.refresh),
+      ),
+    );
+  }
+}
+
+
+
