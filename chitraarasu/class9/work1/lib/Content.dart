cf1a3a0840d64c357a281db67b57dfc40cb3a88d
chitraarasu <kchitraarasu@gmail.com>
2021-11-27 11:12:39 +0530

    reusable button

--- chitraarasu/class9/work1/lib/Content.dart
+++ chitraarasu/class9/work1/lib/Content.dart
@@ -21,10 +21,10 @@ class _ContentState extends State<Content> {
 
   var clickedButton = [];
 
-  setText(int a) {
+  setText(String a) {
     setState(() {
       count++;
-      text = "${buttonList["but$a"]}";
+      text = a;
       clickedButton.add(text);
       if (count == 4) {
         Toast.show(
@@ -33,41 +33,39 @@ class _ContentState extends State<Content> {
         );
         clickedButton = [];
         count = 0;
+        text = "";
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    var keys = buttonList.keys;
+    var values = buttonList.values;
+
+    Widget ButtonGen() {
+      List<Button> buttonListShow = [];
+      for (int a = 0; a < buttonList.length; a++) {
+        buttonListShow.add(
+          Button(
+            keys.elementAt(a),
+            () {
+              setText(values.elementAt(a));
+            },
+          ),
+        );
+      }
+      return Column(
+        children: buttonListShow,
+      );
+    }
+
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(text),
-          Button(
-            "one",
-            () {
-              setText(1);
-            },
-          ),
-          Button(
-            "two",
-            () {
-              setText(2);
-            },
-          ),
-          Button(
-            "three",
-            () {
-              setText(3);
-            },
-          ),
-          Button(
-            "four",
-            () {
-              setText(4);
-            },
-          ),
+          ButtonGen(),
         ],
       ),
     );
