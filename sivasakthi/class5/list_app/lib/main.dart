050bf36c8fbc4b36d75bd69fe5e49f33d5553c88
Ponneelan <92022741+Ponneelan@users.noreply.github.com>
2021-12-04 21:52:17 +0530

    Update main.dart
    conflicts  resolve

--- sivasakthi/class5/list_app/lib/main.dart
+++ sivasakthi/class5/list_app/lib/main.dart
@@ -1,35 +1,66 @@
-import 'package:flutter/material.dart';
-import 'dart:ui';
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-
-}
-}
-class ListViewBuilder extends StatelessWidget {
-
-
-			
-      leading: CircleAvatar(),
-			
-			title:Text("List item $index"),
-      onTap: () {
-                    Navigator.push(
-                        con,
-                        MaterialPageRoute(
-                            builder: (cc) => OtherScreen(value: '$Hi')));
-                  }
+import 'package:flutter/material.dart';  
       
-			);
+    void main() {  
+      runApp(MyApp());  
+    }  
       
-		}
-		),
-	);
-}
+    class MyApp extends StatefulWidget {  
+      @override  
+      _MyAppState createState() => _MyAppState();  
+    }  
 
-  OtherScreen({required String value}) {}
-}
 
-    );
-  }
-}
+    
+      
+    class _MyAppState extends State<MyApp> { 
+      int count=0; 
+      @override  
+      Widget build(BuildContext context) {  
+        return MaterialApp(  
+          home: Scaffold(  
+              appBar: AppBar(  
+                title: Text('INCREMENT-DECREMENT'),
+                backgroundColor: Colors.yellow,  
+              ),  
+          
+              body: Center(
+                child: 
+                Column(children: [
+                  Text("$count"),
+                  Row(children: <Widget>[  
+                Container(  
+                  margin: EdgeInsets.all(25),  
+                  child: FlatButton(  
+                    child: Text('INC', style: TextStyle(fontSize: 20.0),), 
+                    color: Colors.redAccent,
+              
+                    onPressed: () {
+                      setState(() {
+                        count++;
+                      });
+                    }, 
+
+                  ),  
+                ),  
+                Container(  
+                  margin: EdgeInsets.all(25),  
+                  child: FlatButton(  
+                    child: Text('DEC', style: TextStyle(fontSize: 20.0),),  
+                    color: Colors.greenAccent,  
+                    textColor: Colors.white,  
+                    onPressed: () {
+                      setState(() {
+                        count--;
+                      });
+                    },  
+                  ),  
+                ),  
+              ]  
+             )  
+                ],)
+                
+                
+          ),  
+        ));  
+      }  
+    }  
