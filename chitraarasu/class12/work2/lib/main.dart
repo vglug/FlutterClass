90c658fc8784e3ec420cca0b81bfb73e3201c30c
chitraarasu <kchitraarasu@gmail.com>
2021-12-12 18:08:42 +0530

    class12 Api call

---
+++ chitraarasu/class12/work2/lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        fontFamily: 'Georgia',
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var data = null;
+
+  getData() async {
+    http.Response response = await http.get(Uri.parse(
+        "https://raw.githubusercontent.com/ViluppuramGLUG/FlutterClass/main/vglug/test.json"));
+    if (response.statusCode == 200) {
+      var rawdata = response.body;
+      var rdata = await jsonDecode(rawdata);
+      setState(() {
+        data = rdata;
+      });
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: data == null ? Text("") : Text(data["name"]),
+        centerTitle: true,
+      ),
+      body: data == null
+          ? Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.network(
+                    data["image"],
+                    width: 200.0,
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Text(data["location"]),
+                  Text(data["pincode"]),
+                ],
+              ),
+            ),
+    );
+  }
+}
